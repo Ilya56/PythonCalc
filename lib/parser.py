8f234d2aaf9f01112ac294f8629dace0e12642e3
smithros <kovalr2000@gmail.com>
2019-03-11 17:00:27 +0200

    made numbers from parser floats

--- lib/parser.py
+++ lib/parser.py
@@ -5,27 +5,29 @@ def parser(line: str):
 
     if '+' in line:
         a = line.split('+')
-        res += ['add', a[0], a[1]]
+        res += ['add', float(a[0]), float(a[1])]
     elif '-' in line:
         a = line.split('-')
-        res += ['sub', a[0], a[1]]
+        res += ['sub', float(a[0]), float(a[1])]
     elif '*' in line:
         a = line.split('*')
-        res += ['mul', a[0], a[1]]
+        res += ['mul', float(a[0]), float(a[1])]
     elif '/' in line:
         a = line.split('/')
-        res += ['div', a[0], a[1]]
+        res += ['div', float(a[0]), float(a[1])]
     elif '^' in line:
         a = line.split('^')
-        res += ['pow', a[0], a[1]]
+        res += ['pow', float(a[0]), float(a[1])]
     elif 'ln' in line:
         a = line.split('ln')
-        res += ['ln', a[1]]
+        res += ['ln', float(a[1])]
     elif 'lg' in line:
         a = line.split('lg')
-        res += ['lg', a[1]]
+        res += ['lg', float(a[1])]
     elif 'log' in line:
         a = line.split('log')
         a2 = a[1].split(",")
-        res += ['log', a2[0], a2[1]]
+        res += ['log', float(a2[0]), float(a2[1])]
+    else:
+        raise Exception("OperationNotFound")
     return res
